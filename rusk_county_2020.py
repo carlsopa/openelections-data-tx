9d5c9cd1171c14696446923e8064254107c526c5
Paul R. Carlson <carlsonpar@gmail.com>
2020-07-15 11:41:55 -0400

    comment clean up

--- rusk_county_2020.py
+++ rusk_county_2020.py
@@ -1,10 +1,3 @@
-#left to right: 215mm
-#top: 30
-#bottom: 260
-#left 0
-#right 210
-#Registered Voters, Ballots Cast, Straight Party, President, US Senate, US House, Railroad Commissioner, State Senate, State Represenative
-
 import tabula
 import pandas as pd
 import numpy as np
@@ -14,7 +7,7 @@ headers = ['county','precinct','district','party','candidate','votes']
 voting_style = ['early_voting','election_day','provisional','mail']
 result = []
 
-data = tabula.read_pdf(pdf_file,guess=False,multiple_tables=True,pages=('1-9'))
+data = tabula.read_pdf(pdf_file,guess=False,multiple_tables=True,pages=('all'))
 for x in data:
     index = 0
     df = x
@@ -24,28 +17,31 @@ for x in data:
     
     #check to see if the data frame is the start of a new precinct.  If so, then remove the overall statistic data from it.
     if 'STATISTICS' in str(df.iloc[:,0][3]):
-        df.drop(df.index[0:5],inplace=True)
+        df = df.drop(df.index[0:5])
         df[['total','mail']] = df['Unnamed: 1'].str.split(expand=True)
         df.reindex(columns=['Summary Results Report','Unnamed: 0','mail','Unnamed: 2','Unnamed: 3','total','Unnamed: 1'])
         df['Unnamed: 0'] = df['Unnamed: 0'].combine_first(df['total'])
-        df.drop(columns=['Unnamed: 1','total'],inplace=True)
-    df.reset_index(drop=True,inplace=True)
+        df = df.drop(columns=['Unnamed: 1','total'])
+        df = df.rename(columns={
+            'Unnamed: 0':'total',
+            'Unnamed: 2':'early voting',
+            'Unnamed: 3':'election day'
+        })
+    df = df.reset_index(drop=True)
     
     #clean up the first column, remove unneeded rows, and also any rows that have no values in it
     remove_strings=['Vote For 1','TOTAL']
-    result = df.index[df['Summary Results Report'].isin(remove_strings)].tolist()
-    df.drop(df.index[result],inplace=True)
-    df.dropna(subset=['Summary Results Report'],inplace=True)    
-    df.dropna(axis='columns',how='all',inplace=True)
-
-
-  #Unnamed: 0 = Total, 
-    #df.rename(columns={'Unnamed: 0':'candidate','Unnamed: 2':'votes','Ballots By':'mail','Unnamed: 3':'early_voting','Election':'election_day'},inplace=True)
-
-    #df = df.drop([0,1])
+    remove_strings_list = df.index[df['Summary Results Report'].isin(remove_strings)].tolist()
+    df = df.drop(df.index[remove_strings_list])
+    df = df.dropna(subset=['Summary Results Report'])    
+    df = df.dropna(axis='columns',how='all')
+    df = df.rename(columns={
+        'Unnamed: 0':'total',
+        'Unnamed: 2':'mail',
+        'Unnamed: 3':'early voting',
+        'Unnamed: 4':'election day'
+    })
+    df = df.reset_index(drop=True)
     df = df.dropna(how='all')
-    #df = df.dropna(subset=['candidate'])
-    print(df)
     result.append(df)
-#print(data)
-#pd.concat(result).to_csv('pda.csv')
+pd.concat(result).to_csv('pda.csv')
